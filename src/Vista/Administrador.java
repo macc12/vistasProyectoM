16036177c8633afdc585b2de380de29635a1dcf0
LabingXEON <LabingXEON@LabingXEON8-PC>
2018-08-18 17:42:31 -0500

    4

--- src/Vista/Administrador.java
+++ src/Vista/Administrador.java
@@ -4,6 +4,7 @@ import java.beans.EventHandler;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -26,7 +27,7 @@ import javafx.stage.Stage;
 public class Administrador extends Application{
     private GridPane pane;
     private Scene scene;
-    private BorderPane root;
+    private VBox root;
     private VBox vbox;
     private HBox hbox;
     private Button btn_hom;
@@ -43,12 +44,25 @@ public class Administrador extends Application{
     private GridPane pane2;
     private TextField txt_nomb;
     private TextField txt_fecha;
+    private HBox hbox2;
+    private TextField txt_produc;
+    private Button btn_buscar;
+    private TextField txt_cantidad;
+    private Button btn_agreg;
+    private TextField txt_dinero;
+    private Button btn_pago;
+    private TextField txt_cambio;
+    private TextField txt_total;
+    private Button btn_eliminar;
+    private Button btn_imprimir;
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
-        root = new BorderPane();
+        root = new VBox();
         
-        root.setTop(getHBox());
-        root.setCenter(getVBox());
+        root.getChildren().add(getHBox());
+        root.getChildren().add(getVBox());
+        root.getChildren().add(getHBox2());
         
         
         scene = new Scene(root, 1000, 700);
@@ -82,6 +96,8 @@ public class Administrador extends Application{
         pane2 = new GridPane();
         txt_fecha = new TextField();
         txt_nomb = new TextField();
+        txt_nomb.setEditable(false);
+        txt_fecha.setEditable(false);
         pane2.add(new Label("Nombre"), 0, 0);
         pane2.add(txt_nomb, 1, 0);
         pane2.add(new Label("Fecha"), 0, 1);
@@ -118,7 +134,62 @@ public class Administrador extends Application{
         
         return hbox;
     }
-    
+    /*
+    -------------------AQUI---------------------
+    -------------------AQUI---------------------
+    -------------------AQUI---------------------
+    -------------------AQUI---------------------
+    */
+    public HBox getHBox2(){
+        GridPane panei = new GridPane();
+        panei.setHgap(10);
+        panei.setVgap(10);        
+        hbox2 = new HBox();                
+        txt_produc = new TextField();
+        btn_buscar = new Button("Buscar");
+        txt_cantidad = new TextField();
+        btn_agreg = new Button("Agregar");
+        
+        panei.add(new Label("Codigo Producto"), 0, 1);
+        panei.add(txt_produc, 1, 1);
+        panei.add(btn_buscar, 2, 1);
+        panei.add(new Label("Cantidad Producto"), 0, 3);
+        panei.add(txt_cantidad, 1, 3);
+        panei.add(btn_agreg, 2, 3);
+        
+        GridPane panec = new GridPane();
+        panec.setHgap(10);
+        panec.setVgap(10);
+        txt_cantidad = new TextField();
+        btn_pago = new Button("Pago");
+        txt_cambio = new TextField();
+        txt_cambio.setEditable(false);
+        
+        panec.add(new Label("Dinero"), 0, 1);
+        panec.add(txt_cantidad, 1, 1);
+        panec.add(btn_pago, 2, 1);
+        panec.add(new Label("Cambio"), 0, 3);
+        panec.add(txt_cambio, 1, 3);
+        
+        GridPane paned = new GridPane();
+        paned.setHgap(10);
+        paned.setVgap(10);
+        txt_total = new TextField();
+        btn_eliminar = new Button("    Eliminar    ");
+        btn_imprimir = new Button("    Imprimir    ");
+        
+        paned.add(new Label("Total"), 1, 1);
+        paned.add(txt_total, 2, 1);
+        paned.add(btn_eliminar, 2, 3);
+        paned.add(btn_imprimir, 2, 5);
+        
+        hbox2.getChildren().add(panei);
+        hbox2.getChildren().add(panec);
+        hbox2.getChildren().add(paned);
+        
+        
+        return hbox2;
+    }
     
     public static void main(String[] args) {
         Application.launch(args);
@@ -142,5 +213,11 @@ public class Administrador extends Application{
     }
     public void clearTable() {
         table.getItems().clear();
+    }
+    public void addNombre(String nombre){
+        txt_nomb.setText(nombre);
+    }
+    public void addFecha(String fecha){
+        txt_fecha.setText(fecha);
     }
 }
